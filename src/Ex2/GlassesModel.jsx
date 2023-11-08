e0b11304bc30ae3229e5e3b409aaf527bcf50870
MinhNghia1 <leminhnghia1382002@gmmail.com>
2023-11-08 21:01:49 +0700

    done

--- src/Ex2/GlassesModel.jsx
+++ src/Ex2/GlassesModel.jsx
@@ -1,10 +1,18 @@
 import React from "react";
-
+import "../asset/style/style.css";
 export default function GlassesModel({ glass }) {
   return (
-    <div className="text-center mt-5">
-      <img src="./glassesImage/model.jpg" alt="model.jpg" height={300} />
-      <img src={glass.url} alt="g1.jpg" height={40} />
+    <div>
+      <div className="text-center mt-5 ">
+        <div className="imagesclasses">
+          {/* <div className="imageModel"> */}
+          <img src="./glassesImage/model.jpg" alt="model.jpg" height={300} />
+          {/* </div> */}
+          {/* <div className="imageItem"> */}
+          <img className="imageItem" src={glass.url} alt="g1.jpg" height={40} />
+          {/* </div> */}
+        </div>
+      </div>
     </div>
   );
 }
